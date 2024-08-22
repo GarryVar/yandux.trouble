6cc3c105344c5faceaff85cda3420ff30fb0ab13
garryvar <darisvet.sokolov@yandex.ru>
2024-08-22 22:06:20 +0300

    Fix script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,12 @@ function addTabHandler(evt) {
     let newTab = document.createElement('a');
     newTab.href="#";
     newTab.classList.add('tab__item', 'animate__animated', 'animate__fadeIn');
-    btnMore.insertAdjacentElement('beforebegin', newTab)
+    if (btnMore.nodeName === 'BUTTON') {
+        btnMore.insertAdjacentElement('beforebegin', newTab)
+    }
+    else {
+        return
+    }
 }
 
 window.addEventListener('click', addTabHandler);
